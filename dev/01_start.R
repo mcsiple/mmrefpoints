d432cf3bf89457cb6167fa2fb27f3659401e4a77
mcsiple <mcsiple@gmail.com>
2021-03-05 07:54:30 -0800

    test

--- dev/01_start.R
+++ dev/01_start.R
@@ -16,26 +16,27 @@
 golem::fill_desc(
   pkg_name = "mmrefpoints", # The Name of the package containing the App 
   pkg_title = "Project Marine Mammal Populations and Calculate Reference Points", # The Title of the package containing the App 
-  pkg_description = "This package contains tools for projecting marine mammal populations into the future under different bycatch scenarios. It uses an age-structured population model to project numbers at age, based on life history parameters and some reference points entered by the user. Its primary purpose is to allow users to explore the long term impacts of different bycatch rates, based on some basic life history information. It is also built to help users understand and estimate reference points for bycatch such as Potential Biological Removal (PBR).", # The Description of the package containing the App 
+  pkg_description = "Tools for projecting marine mammal populations into the future under different bycatch scenarios. It uses an age-structured population model to project numbers at age, based on life history parameters and some reference points entered by the user. Its primary purpose is to allow users to explore the long term impacts of different bycatch rates, based on some basic life history information. It is also built to help users understand and estimate reference points for bycatch such as Potential Biological Removal (PBR).", # The Description of the package containing the App 
   author_first_name = "Margaret", # Your First Name
   author_last_name = "Siple", # Your Last Name
   author_email = "margaret.siple@noaa.gov", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
-)     
+) #done
 
 ## Set {golem} options ----
 golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_mit_license(copyright_holder = "Margaret C. Siple" )  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
 usethis::use_news_md( open = FALSE )
 
 ## Use git ----
-usethis::use_git()
+usethis::use_git_config(user.name = "mcsiple", user.email = "mcsiple@gmail.com")
+#usethis::use_git() # doesn't point to the right directory!
 
 ## Init Testing Infrastructure ----
 ## Create a template for tests
