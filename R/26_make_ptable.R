2296e876937ef9853905993a600d156764f36a47
mcsiple <mcsiple@gmail.com>
2021-03-05 20:55:20 -0800

    update prob_rebuilt_goal() and abund_rel_vec()

--- R/26_make_ptable.R
+++ R/26_make_ptable.R
@@ -10,15 +10,15 @@
 #'
 make_ptable <- function(traj.list, depletion, lh.params, mnpl) {
   # Each of these is one depletion level
-  HiDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob.rebuilt.goal(traj = x[[1]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
+  HiDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[1]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
   # This is a list of the probability of recovery for each bycatch level, for the first depletion level
-  MedDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob.rebuilt.goal(traj = x[[2]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
-  LowDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob.rebuilt.goal(traj = x[[3]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
+  MedDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[2]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
+  LowDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[3]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
 
   # Same three depletion levels:
-  HiDepl2 <- unlist(lapply(X = traj.list, FUN = function(x) prob.rebuilt.goal(traj = x[[1]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 100)))
-  MedDepl2 <- unlist(lapply(X = traj.list, FUN = function(x) prob.rebuilt.goal(traj = x[[2]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 100)))
-  LowDepl2 <- unlist(lapply(X = traj.list, FUN = function(x) prob.rebuilt.goal(traj = x[[3]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 100)))
+  HiDepl2 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[1]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 100)))
+  MedDepl2 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[2]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 100)))
+  LowDepl2 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[3]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 100)))
 
   # make the table
   bycatch <- c("high", "med", "low", "zero")
@@ -28,9 +28,9 @@ make_ptable <- function(traj.list, depletion, lh.params, mnpl) {
     add_column(
       prebuild50 = c(HiDepl1, MedDepl1, LowDepl1),
       prebuild100 = c(HiDepl2, MedDepl2, LowDepl2),
-      abundrel10 = abund.rel.vec(traj.list = traj.list, K = lh.params$K, years.vec = 10),
-      abundrel20 = abund.rel.vec(traj.list = traj.list, K = lh.params$K, years.vec = 20),
-      abundrel50 = abund.rel.vec(traj.list = traj.list, K = lh.params$K, years.vec = 50)
+      abundrel10 = abund_rel_vec(traj.list = traj.list, K = lh.params$K, years.vec = 10),
+      abundrel20 = abund_rel_vec(traj.list = traj.list, K = lh.params$K, years.vec = 20),
+      abundrel50 = abund_rel_vec(traj.list = traj.list, K = lh.params$K, years.vec = 50)
     )
 
   return(ptable)
