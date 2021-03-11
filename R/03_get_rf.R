707cae31282aecf348afc9290862771671876dec
mcsiple <mcsiple@gmail.com>
2021-03-11 13:59:17 -0800

    added example, changed title

--- R/03_get_rf.R
+++ R/03_get_rf.R
@@ -1,4 +1,4 @@
-#' Get recruitment at bycatch mortality rate E
+#' Get relative recruitment at bycatch mortality rate E
 #'
 #' @description calculates recruitment at bycatch mortality rate \emph{E}
 #'
@@ -15,6 +15,17 @@
 #'
 #' @return recruitment given exploitation rate \emph{E} - this value is multiplied by the initial abundance \eqn{N_{init}} to get initial nums at age ( a vector)
 #'
+#' @examples 
+#' S0 = 0.944; S1plus = 0.99; nages = 25; AgeMat = 17; z = 2.39; lambdaMax = 1.04;
+#' NPROut <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = 0)
+#' N0 <- NPROut$npr # mature numbers per recruit
+# Get number of individuals per recruit in terms of individuals aged 1+ (\eqn{P0.w})
+#' P0 <- NPROut$P1r # 1+ nums per recruit
+#' Fec0 <- 1.0 / N0
+#' FecMax <- getfecmax(S1plus = S1plus, S0 = S0, AgeMat = AgeMat, lambdaMax = lambdaMax)
+#' A <- (FecMax - Fec0) / Fec0
+#' get_rf(E_in = 0.01, S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A,P0 = P0,N0 = N0)
+#' 
 #' @export
 get_rf <- function(E_in, S0, S1plus, nages, AgeMat, z, A, P0, N0) {
   NE <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = E_in)$npr
