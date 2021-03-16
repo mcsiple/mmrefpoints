ec78b75ce3ab9062699e8844c425764ac002856d
mcsiple <mcsiple@gmail.com>
2021-03-15 22:43:16 -0700

    if user doesn't specify mnpl, calculate it

--- R/26_make_ptable.R
+++ R/26_make_ptable.R
@@ -3,12 +3,20 @@
 #' @param traj.list A list of trajectories from \code{Projections()}
 #' @param depletion A vector of starting depletions (abundance relative to carrying capacity)
 #' @param lh.params A list of life history parameters
-#' @param mnpl Max net productivity level (defined by user)
+#' @param mnpl Max net productivity level (MNPL). If MNPL is specified, that value is used. If the user does not specify MNPL, it is calculated from the life history parameters.
 #'
-#' @return
+#' @return A dataframe containing performance metrics. 
+#' 
+#' @examples
 #' @export
 #'
 make_ptable <- function(traj.list, depletion, lh.params, mnpl) {
+  
+  # If the user hasn't provided MNPL, calculate it.
+  # if(is.na(mnpl)){
+  #   mnpl <- get_mnpl(E.start = 0.001, lh.params = lh.params)
+  # }
+  # 
   # Each of these is one depletion level
   HiDepl1 <- unlist(lapply(X = traj.list, FUN = function(x) prob_rebuilt_goal(traj = x[[1]][["trajectories"]], goal = mnpl * lh.params$K1plus, rebuild.yr = 50)))
   # This is a list of the probability of recovery for each bycatch level, for the first depletion level
@@ -23,7 +31,7 @@ make_ptable <- function(traj.list, depletion, lh.params, mnpl) {
   # make the table
   bycatch <- c("high", "med", "low", "zero")
   df <- expand_grid(bycatch, depletion)
-
+#browser()
   ptable <- expand_grid(depletion, bycatch) %>%
     add_column(
       prebuild50 = c(HiDepl1, MedDepl1, LowDepl1),
