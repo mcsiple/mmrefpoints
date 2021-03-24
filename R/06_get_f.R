ea3bee346798ea1dadad62a005ac6982599d57f0
mcsiple <mcsiple@gmail.com>
2021-03-23 19:51:32 -0700

    remove K1plus.w from function call

--- R/06_get_f.R
+++ R/06_get_f.R
@@ -5,7 +5,6 @@
 #' @param f.start an initial guess for the bycatch mortality rate E. The default value is E = 0.5
 #' @param S0.w Calf/pup survival, a numeric value between 0 and 1. (Note: the 'w' suffix indicates that \eqn{z} is in the wrapper function, and is used inside the function by \code{optim})
 #' @param S1plus.w  survival rate for animals age 1 year and older (a numeric value between 0 and 1)
-#' @param K1plus.w The pre-exploitation population size of individuals aged 1 and older. If this value is unavailable, it can be approximated by using the initial depletion and the estimate of current abundance.
 #' @param AgeMat.w Age at maturity in years (assumed to be age at first parturition - 1)
 #' @param nages.w "maximum" age, treated as the plus group age. The plus group age can be set equal to the age at maturity +2 years without losing accuracy.
 #' @param InitDepl.w The depletion level to solve for (this is equivalent to 'starting depletion')
@@ -19,7 +18,7 @@
 #'
 #' @examples
 #' Set parameters
-#' S0.w = 0.5; S1plus.w = 0.944; nages.w = 25; K1plus.w = 9000; AgeMat.w = 18 
+#' S0.w = 0.5; S1plus.w = 0.944; nages.w = 25; AgeMat.w = 18 
 #' InitDepl.w = 0.9; z.w = 2.39; lambdaMax.w = 1.04
 #' # Get number of individuals per recruit in terms of mature individuals (\eqn{N0.w})
 #' NPROut <- npr(S0 = S0.w, S1plus = S1plus.w, nages = nages.w, AgeMat = AgeMat.w, E = 0)
