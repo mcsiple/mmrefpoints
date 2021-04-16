6649a97bcd556fba719e62165394e3add381af62
mcsiple <mcsiple@gmail.com>
2021-04-16 15:15:40 -0700

    extreme cleanup; add dat before yield curves and new base R version of plot

--- R/13_pop_vs_yield.R
+++ R/13_pop_vs_yield.R
@@ -63,45 +63,47 @@ pop_vs_yield <- function(z.vec = c(1, 2.39, 5.99),
   )
   A <- (fmax - Fec0) / Fec0
   E.vec <- seq(0, 0.1, length.out = 100)
+
+  # paste in
+  z.list <- list()
+  for (j in 1:length(z.vec)) {
+    z <- z.vec[j]
+    yield.vec <- vector()
+    rel1plus <- vector()
+
+    # unfished nums per recruit
+    u1p <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = 0)$P1r *
+      get_rf(E_in = 0, S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A, P0 = P0, N0 = N0) # == 1
+
+    for (i in 1:length(E.vec)) {
+      # yield at exploitation E
+      yield.vec[i] <- ce(
+        S0 = S0,
+        S1plus = S1plus,
+        nages = nages,
+        AgeMat = AgeMat,
+        z = z,
+        E = E.vec[i], A = A, P0 = P0, N0 = N0
+      )
+
+      # nums per recruit at exploitation E
+      n1p <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = E.vec[i])$P1r *
+        get_rf(E_in = E.vec[i], S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A, P0 = P0, N0 = N0)
+
+      # relative 1+ pop size at E
+      rel1plus[i] <- n1p / u1p
+    }
+    if (j == 1) {
+      maxyield <- which.max(yield.vec)
+      print(paste("approx MNPL", round(rel1plus[maxyield], digits = 2)))
+      print(paste("approx FMSY", round(E.vec[maxyield], digits = 5)))
+    }
+
+    z.list[[j]] <- data.frame(z = z, E = E.vec, yield = yield.vec, rel1p = rel1plus)
+  } # / j loop
+  dat <- bind_rows(z.list, .id = "column_label")
+
   if (ggp) {
-    z.list <- list()
-    for (j in 1:length(z.vec)) {
-      z <- z.vec[j]
-      yield.vec <- vector()
-      rel1plus <- vector()
-
-      # unfished nums per recruit
-      u1p <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = 0)$P1r *
-        get_rf(E_in = 0, S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A, P0 = P0, N0 = N0) # == 1
-
-      for (i in 1:length(E.vec)) {
-        # yield at exploitation E
-        yield.vec[i] <- ce(
-          S0 = S0,
-          S1plus = S1plus,
-          nages = nages,
-          AgeMat = AgeMat,
-          z = z,
-          E = E.vec[i], A = A, P0 = P0, N0 = N0
-        )
-
-        # nums per recruit at exploitation E
-        n1p <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = E.vec[i])$P1r *
-          get_rf(E_in = E.vec[i], S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A, P0 = P0, N0 = N0)
-
-        # relative 1+ pop size at E
-        rel1plus[i] <- n1p / u1p
-      }
-      if(j==1){
-        maxyield <- which.max(yield.vec)
-        print(paste("approx MNPL", round(rel1plus[maxyield], digits = 2)))
-        print(paste("approx FMSY", round(E.vec[maxyield], digits = 5)))
-      }
-      
-      z.list[[j]] <- data.frame(z = z, E = E.vec, yield = yield.vec, rel1p = rel1plus)
-    } #/ j loop
-    dat <- bind_rows(z.list, .id = "column_label")
-
     p <- dat %>%
       filter(z == z.vec[1]) %>%
       ggplot(aes(x = rel1p, y = yield)) +
@@ -113,51 +115,33 @@ pop_vs_yield <- function(z.vec = c(1, 2.39, 5.99),
         axis.title.x = element_text(size = 20),
         axis.title.y = element_text(size = 20)
       ) +
-      scale_x_continuous(limits = c(0, 1.5)) +
+      scale_x_continuous(limits = c(0, 1.5), expand = c(0, 0)) +
+      scale_y_continuous(expand = c(0, 0)) +
       xlab(xlab1) + # "Depletion \n(population size relative to K)",
       ylab(ylab1) # Production
-  
     return(p)
   } else {
+    print("should be base r!")
     plot(0:1, 0:1,
       type = "n",
       xlab = xlab1,
-      ylab = ylab1, ylim = c(0, 1.5), xlim = c(0, 2)
+      ylab = ylab1,
+      xlim = c(0, 1.5),
+      ylim = c(0, max(dat$yield))
     )
-
-    for (j in 1:length(z.vec)) {
-      z <- z.vec[j]
-      yield.vec <- vector()
-      rel1plus <- vector()
-
-      # unfished nums per recruit
-      u1p <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = 0)$P1r *
-        get_rf(E_in = 0, S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A, P0 = P0, N0 = N0) # == 1
-
-      for (i in 1:length(E.vec)) {
-        # yield at exploitation E
-        yield.vec[i] <- ce(
-          S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z,
-          E = E.vec[i], A = A, P0 = P0, N0 = N0
-        )
-        # nums per recruit at exploitation E
-        n1p <- npr(S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, E = E.vec[i])$P1r *
-          get_rf(E_in = E.vec[i], S0 = S0, S1plus = S1plus, nages = nages, AgeMat = AgeMat, z = z, A = A, P0 = P0, N0 = N0)
-        # relative 1+ pop size at E
-        rel1plus[i] <- n1p / u1p
-      }
-      lines(rel1plus[1:n2p], yield.vec[1:n2p], col = "darkblue")
-      maxyield <- which.max(yield.vec)
-     
-      print(paste("approx MNPL", round(rel1plus[maxyield], digits = 2)))
-      print(paste("approx FMSY", round(E.vec[maxyield], digits = 5)))}
-    
-    if (add.legend) {
-      legend("topright",
-        lty = c(1, 1, 1),
-        col = cols,
-        legend = c("z = 1", "z = 2.39", "z = 5.99")
-      )
+    maxyield <- MNPL <- FMSY <- vector()
+    for (z in 1:length(z.vec)) {
+      col_vec <- c("lightblue", "blue", "darkblue")
+      x_values <- dat[which(dat$column_label == z), "rel1p"]
+      y_values <- dat[which(dat$column_label == z), "yield"]
+      lines(x_values[1:n2p], y_values[1:n2p], col = col_vec[z])
+      maxyield[z] <- which.max(y_values)
+      MNPL[z] <- x_values[maxyield][1]
+      FMSY[z] <- dat[which(dat$column_label == z)[maxyield[z]], "E"][1]
     }
-  } # end base R version
+    MNPL <- round(MNPL, digits = 2)
+    FMSY <- round(FMSY, digits = 2)
+    legend("topright", legend = paste0("z = ", z.vec, "\napprox MNPL = ", MNPL, "\napprox FMSY = ", FMSY, "\n"), lty = 1, col = col_vec, box.lwd = 0)
+    return(data.frame(z = z.vec, MNPL = MNPL, FMSY = FMSY))
+  } # /base R
 }
