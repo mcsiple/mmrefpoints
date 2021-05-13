bfb8da93ae698f4f02f5818d2a8ffcd9f4e8b222
mcsiple <mcsiple@gmail.com>
2021-05-13 14:23:15 -0700

    fixed example

--- R/26_make_ptable.R
+++ R/26_make_ptable.R
@@ -7,9 +7,10 @@
 #' @return A dataframe containing performance metrics. 
 #' 
 #' @examples
-#' parms <- list(S0 = 0.944, S1plus = 0.99, K1plus = 9000, AgeMat = 18, 
+#' parms <- list(S0 = 0.944, S1plus = 0.99, K1plus = 9000, AgeMat = 18, nages = 20,
 #'               z = 2.39, lambdaMax = 1.02)
 #' initdepl.vec <- c(0.2, 0.5, 0.9)
+#' nyears <- 100
 #' high.list.const <- lapply(
 #'   X = initdepl.vec,
 #'   function(x) {
