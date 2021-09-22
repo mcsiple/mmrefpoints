030e0320337d0ab1e193c8b0a1b6adbe0a1a4a63
mcsiple <mcsiple@gmail.com>
2021-09-22 10:40:21 -0700

    Edit text for finesse :zap:

--- dev/01_start.R
+++ dev/01_start.R
@@ -16,7 +16,7 @@
 golem::fill_desc(
   pkg_name = "mmrefpoints", # The Name of the package containing the App 
   pkg_title = "Project Marine Mammal Populations and Calculate Reference Points", # The Title of the package containing the App 
-  pkg_description = "Tools for projecting marine mammal populations into the future under different bycatch scenarios. It uses an age-structured population model to project numbers at age, based on life history parameters and some reference points entered by the user. Its primary purpose is to allow users to explore the long term impacts of different bycatch rates, based on some basic life history information. It is also built to help users understand and estimate reference points for bycatch such as Potential Biological Removal (PBR).", # The Description of the package containing the App 
+  pkg_description = "This contain tools for projecting marine mammal populations into the future under different bycatch scenarios. It uses an age-structured population model to project numbers at age, based on life history parameters and some reference points entered by the user. Its primary purpose is to allow users to explore the long term impacts of different bycatch rates, based on some basic life history information. It is also built to help users understand and estimate reference points for bycatch such as Potential Biological Removal (PBR).", # The Description of the package containing the App 
   author_first_name = "Margaret", # Your First Name
   author_last_name = "Siple", # Your Last Name
   author_email = "margaret.siple@noaa.gov", # Your Email
