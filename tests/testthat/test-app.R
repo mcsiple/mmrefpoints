f5a060ddc1511347684ad94c5fd29946c5e61b0d
mcsiple <mcsiple@gmail.com>
2021-09-29 14:40:04 -0700

    add test for spanish language change

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,3 +1,14 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+ test_that("reactives and output updates",{
+            testServer(app_server, {
+             session$setInputs(selected_language = "es",
+                               Nmin.usr = 1000, fr.usr = 0.5, Rmax.usr = 1.01)
+              x <- output$PBRprint
+              y <- substr(x,1,10)
+              expect_equal(y, "Rmax debe ")
+            })
+  })
+
