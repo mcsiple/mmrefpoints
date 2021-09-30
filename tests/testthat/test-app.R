42466ef2f211a1e21fb5a17ec590100b7a42f65b
mcsiple <mcsiple@gmail.com>
2021-09-30 10:07:48 -0700

    add language switch check to tests

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -2,13 +2,28 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
- test_that("reactives and output updates",{
-            testServer(app_server, {
-             session$setInputs(selected_language = "es",
-                               Nmin.usr = 1000, fr.usr = 0.5, Rmax.usr = 1.01)
-              x <- output$PBRprint
-              y <- substr(x,1,10)
-              expect_equal(y, "Rmax debe ")
-            })
+test_that("Language check with switch", {
+  testServer(app_server, {
+    session$setInputs(
+      selected_language = "es",
+      Nmin.usr = 1000, fr.usr = 0.5, Rmax.usr = 1.01
+    )
+    x <- output$PBRprint
+    y <- substr(x, 1, 10)
+    expect_equal(y, "Rmax debe ")
   })
+})
+
+# test_that("Output exists when MNPL is high", {
+#   testServer(app_server, {
+#     session$setInputs(
+#       type_simple = "bowhead",
+#       MNPL.usr = 0.7
+#     )
+#     x <- output$projPlot1
+#     expect_true(is.ggplot(x))
+#   })
+# })
+
+
 
